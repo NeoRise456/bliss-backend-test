38ead9812e123d5fff59b05f9e03bd34286b5995
Al3xiel <gabriel280104casas@gmail.com>
2024-10-29 02:13:29 -0500

    feat(services): add service query service implementation.

---
+++ NRG3.Bliss.API/ServiceManagement/Application/Internal/QueryServices/ServiceQueryService.cs
@@ -0,0 +1,24 @@
+using NRG3.Bliss.API.ServiceManagement.Domain.Model.Aggregates;
+using NRG3.Bliss.API.ServiceManagement.Domain.Model.Queries;
+using NRG3.Bliss.API.ServiceManagement.Domain.Repositories;
+using NRG3.Bliss.API.ServiceManagement.Domain.Services;
+
+namespace NRG3.Bliss.API.ServiceManagement.Application.Internal.QueryServices;
+
+public class ServiceQueryService(IServiceRepository serviceRepository) : IServiceQueryService
+{
+    public async Task<Service?> Handle(GetServiceByIdQuery query)
+    {
+        return await serviceRepository.FindByIdAsync(query.ServiceId);
+    }
+    
+    public async Task<IEnumerable<Service>> Handle(GetAllServicesQuery query)
+    {
+        return await serviceRepository.ListAsync();
+    }
+    
+    public async Task<IEnumerable<Service>> Handle(GetServicesByCompanyId query)
+    {
+        return await serviceRepository.FindServicesByCompanyIdAsync(query.CompanyId);
+    }
+}
