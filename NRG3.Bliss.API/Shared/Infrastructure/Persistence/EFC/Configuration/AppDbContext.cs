4e32fae88a49a6b0889d2cd2f64ad60161bd2db7
Al3xiel <gabriel280104casas@gmail.com>
2024-10-29 00:21:32 -0500

    feat(orm): add object-relational mapping rules.

--- NRG3.Bliss.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ NRG3.Bliss.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,5 +1,8 @@
 using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
 using Microsoft.EntityFrameworkCore;
+using NRG3.Bliss.API.ServiceManagement.Domain.Model.Aggregates;
+using NRG3.Bliss.API.ServiceManagement.Domain.Model.Entities;
+using NRG3.Bliss.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 
 namespace NRG3.Bliss.API.Shared.Infrastructure.Persistence.EFC.Configuration;
 
@@ -15,5 +18,36 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     {
         base.OnModelCreating(builder);
         //TODO: Add database configuration modeling here
+        
+        builder.Entity<Service>().HasKey(s => s.Id);
+        builder.Entity<Service>().Property(s => s.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Service>().Property(s => s.ServiceName).IsRequired().HasMaxLength(100);
+        builder.Entity<Service>().Property(s => s.Price).IsRequired();
+        builder.Entity<Service>().Property(s => s.Duration).IsRequired();
+        builder.Entity<Service>().Property(s => s.Description).HasMaxLength(500);
+        
+        builder.Entity<Category>().HasKey(c => c.Id);
+        builder.Entity<Category>().Property(c => c.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Category>().Property(c => c.Name).IsRequired().HasMaxLength(50);
+        builder.Entity<Category>().Property(c => c.Description).HasMaxLength(500);
+        
+        builder.Entity<Company>().HasKey(c => c.Id);
+        builder.Entity<Company>().Property(c => c.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Company>().Property(c => c.Name).IsRequired().HasMaxLength(100);
+        builder.Entity<Company>().Property(c => c.Ruc).HasMaxLength(11);
+        
+        builder.Entity<Category>()
+            .HasMany(c => c.Services)
+            .WithOne(s => s.Category)
+            .HasForeignKey(s => s.CategoryId)
+            .HasPrincipalKey(c => c.Id);
+        
+        builder.Entity<Company>()
+            .HasMany(c => c.Services)
+            .WithOne(s => s.Company)
+            .HasForeignKey(s => s.CompanyId)
+            .HasPrincipalKey(c => c.Id);
+        
+        builder.UseSnakeCaseNamingConvention();
     }
 }
