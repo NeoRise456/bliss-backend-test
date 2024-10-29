e817ec231bba4a8d53adaec3769844da96e30fbd
GodofredoGT <u202120772@upc.edu.pe>
2024-10-29 01:20:28 -0500

    feat(company): add company outbound service implementation.

---
+++ NRG3.Bliss.API/ServiceManagement/Infrastructure/Persistence/EFC/Repositories/CompanyRepository.cs
@@ -0,0 +1,11 @@
+using NRG3.Bliss.API.ServiceManagement.Domain.Model.Entities;
+using NRG3.Bliss.API.ServiceManagement.Domain.Repositories;
+using NRG3.Bliss.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using NRG3.Bliss.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace NRG3.Bliss.API.ServiceManagement.Infrastructure.Persistence.EFC.Repositories;
+
+public class CompanyRepository(AppDbContext context) : BaseRepository<Company>(context), ICompanyRepository
+{
+    
+}
