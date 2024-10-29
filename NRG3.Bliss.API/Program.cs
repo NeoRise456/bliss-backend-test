ab32248f988c640b5f7113786f0024838b9e3bcd
Al3xiel <gabriel280104casas@gmail.com>
2024-10-29 02:13:53 -0500

    feat(app) add main application services configuration.

--- NRG3.Bliss.API/Program.cs
+++ NRG3.Bliss.API/Program.cs
@@ -1,13 +1,68 @@
+using Microsoft.EntityFrameworkCore;
+using NRG3.Bliss.API.ServiceManagement.Application.Internal.CommandServices;
+using NRG3.Bliss.API.ServiceManagement.Application.Internal.QueryServices;
+using NRG3.Bliss.API.ServiceManagement.Domain.Repositories;
+using NRG3.Bliss.API.ServiceManagement.Domain.Services;
+using NRG3.Bliss.API.ServiceManagement.Infrastructure.Persistence.EFC.Repositories;
+using NRG3.Bliss.API.Shared.Domain.Repositories;
+using NRG3.Bliss.API.Shared.Infrastructure.Interfaces.ASP.Configuration;
+using NRG3.Bliss.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using NRG3.Bliss.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+// Apply Route Naming Convention
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+builder.Services.AddControllers(options => options.Conventions.Add(new KebabCaseRouteNamingConvention()));
+
+// Add Database Connection
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (connectionString is null)
+    throw new Exception("Connection string is null.");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    if (builder.Environment.IsDevelopment())
+        options.UseMySQL(connectionString)
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            .EnableDetailedErrors()
+            .EnableSensitiveDataLogging();
+    else if (builder.Environment.IsProduction())
+        options.UseMySQL(connectionString);
+    else
+        throw new Exception("Environment is not defined.");
+});
+
+// OpenAPI/Swagger Configuration
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => options.EnableAnnotations());
+
+//Dependency Injection Configuration
+
+//Shared Bounded Context Dependency Injection Configuration
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// Service Management Bounded Context Dependency Injection Configuration
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICompanyRepository, CompanyRepository>();
+builder.Services.AddScoped<IServiceRepository, ServiceRepository>();
+builder.Services.AddScoped<IServiceCommandService, ServiceCommandService>();
+builder.Services.AddScoped<IServiceQueryService, ServiceQueryService>();
 
 var app = builder.Build();
+
+// Verify Database Objects are Created
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,5 +76,3 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
-app.Run();
