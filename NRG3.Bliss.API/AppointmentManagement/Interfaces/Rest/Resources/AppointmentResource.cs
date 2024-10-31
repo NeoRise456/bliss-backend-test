1f8cd0de31781ce4b63e0ffbf18086788f5919ac
NeoRise456 <neorise456@gmail.com>
2024-10-31 00:47:01 -0500

    feat(resources): add appointment resource.

---
+++ NRG3.Bliss.API/AppointmentManagement/Interfaces/Rest/Resources/AppointmentResource.cs
@@ -0,0 +1,14 @@
+﻿using Org.BouncyCastle.Asn1.Cms;
+
+namespace NRG3.Bliss.API.AppointmentManagement.Interfaces.Rest.Resources;
+
+public record AppointmentResource(
+    int id,
+    int userId,
+    int serviceId,
+    int companyId, 
+    DateTime reservationDate,
+    string status, 
+    DateTime date, 
+    string time
+    );
