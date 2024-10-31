aab1c637499fdc47b439ffceff1336d8b0f58d87
cwassointt <dillpixel1@gmail.com>
2024-10-31 02:13:58 -0500

    feat: added new interface for Appointment Repository.

---
+++ NRG3.Bliss.API/AppointmentManagement/Domain/Repositories/IAppointmentRepository.cs
@@ -0,0 +1,10 @@
+﻿using NRG3.Bliss.API.AppointmentManagement.Domain.Model.Aggregates;
+using NRG3.Bliss.API.Shared.Domain.Repositories;
+
+namespace NRG3.Bliss.API.AppointmentManagement.Domain.Repositories;
+
+public interface IAppointmentRepository : IBaseRepository<Appointment>
+{
+    Task<Appointment?> FindByIdAsync(int id);
+    Task<IEnumerable<Appointment>> FindByUserIdAsync(int userId);
+}
