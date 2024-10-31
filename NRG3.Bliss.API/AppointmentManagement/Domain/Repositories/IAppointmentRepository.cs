022fb56b8e7317ccc7424b220d34ab290aa39077
cwassointt <dillpixel1@gmail.com>
2024-10-31 02:25:46 -0500

    fix: fixed task in Appointment Query Service.

--- NRG3.Bliss.API/AppointmentManagement/Domain/Repositories/IAppointmentRepository.cs
+++ NRG3.Bliss.API/AppointmentManagement/Domain/Repositories/IAppointmentRepository.cs
@@ -1,8 +1,12 @@
 ﻿using NRG3.Bliss.API.AppointmentManagement.Domain.Model.Aggregates;
+using NRG3.Bliss.API.Shared.Domain.Repositories;
 
 namespace NRG3.Bliss.API.AppointmentManagement.Domain.Repositories;
 
-public interface IAppointmentRepository
+public interface IAppointmentRepository : IBaseRepository<Appointment>
 {
     Task<IEnumerable<Appointment>> FindAppointmentsByUserIdAsync(int userId);
+    Task<Appointment?> FindAppointmentByIdAsync(int id);
+
+    
 }
