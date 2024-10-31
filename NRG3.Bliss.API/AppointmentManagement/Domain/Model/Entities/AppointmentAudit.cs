951debce61f305dc57341ea83d7bb94286b273cc
cwassointt <dillpixel1@gmail.com>
2024-10-30 23:17:52 -0500

    feat: added new entity for Appointment Audit.

---
+++ NRG3.Bliss.API/AppointmentManagement/Domain/Model/Entities/AppointmentAudit.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NRG3.Bliss.API.AppointmentManagement.Domain.Model.Entities;
+
+public partial class Appointment
+{
+    [Column("CreatedAt")] public DateTimeOffset? CreatedDate { get; set; }
+    [Column("UpdatedAt")] public DateTimeOffset? UpdatedDate { get; set; }
+}
