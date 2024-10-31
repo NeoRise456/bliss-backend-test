bfc11ab512807ccff70573eb4bb75612804d551d
NeoRise456 <neorise456@gmail.com>
2024-10-31 13:31:30 -0500

    feat(repositories): implemented user repository.

---
+++ NRG3.Bliss.API/AppointmentManagement/Infrastructure/Persistence/EFC/Repositories/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿using NRG3.Bliss.API.AppointmentManagement.Domain.Model.Entities;
+using NRG3.Bliss.API.AppointmentManagement.Domain.Repositories;
+using NRG3.Bliss.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using NRG3.Bliss.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace NRG3.Bliss.API.AppointmentManagement.Infrastructure.Persistence.EFC.Repositories;
+
+public class UserRepository(AppDbContext context): BaseRepository<User>(context), IUserRepository
+{
+    
+}
