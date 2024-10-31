96000010acd79af647fd6792a67a738eb7157cd6
cwassointt <dillpixel1@gmail.com>
2024-10-30 23:18:20 -0500

    feat: added commands for Create Appointment.

---
+++ NRG3.Bliss.API/AppointmentManagement/Domain/Model/Commands/CreateAppointmentCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace NRG3.Bliss.API.AppointmentManagement.Domain.Model.Commands;
+
+public record CreateAppointmentCommand(
+    int UserId,
+    int ServiceId,
+    int CompanyId,
+    DateTime ReservationDate,
+    string Status,
+    DateTime Date,
+    string Time
+);
