21ca354fd7be1104474e624aa444f7848ed9a760
cwassointt <dillpixel1@gmail.com>
2024-10-31 02:12:48 -0500

    feat: added new task for Appointment Command Service interface.

---
+++ NRG3.Bliss.API/AppointmentManagement/Domain/Services/IAppointmentCommandService.cs
@@ -0,0 +1,9 @@
+﻿using NRG3.Bliss.API.AppointmentManagement.Domain.Model.Aggregates;
+using NRG3.Bliss.API.AppointmentManagement.Domain.Model.Commands;
+
+namespace NRG3.Bliss.API.AppointmentManagement.Domain.Services;
+
+public interface IAppointmentCommandService
+{
+    Task<Appointment?> Handle(CreateAppointmentCommand command);
+}
