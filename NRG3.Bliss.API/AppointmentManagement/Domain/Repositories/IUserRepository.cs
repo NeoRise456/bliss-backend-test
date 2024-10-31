6ecd2b808a335ea38d28d384a4ca04410aa664f6
NeoRise456 <neorise456@gmail.com>
2024-10-31 13:31:16 -0500

    feat(repositories): added user repository contract.

---
+++ NRG3.Bliss.API/AppointmentManagement/Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using NRG3.Bliss.API.AppointmentManagement.Domain.Model.Entities;
+using NRG3.Bliss.API.Shared.Domain.Repositories;
+
+namespace NRG3.Bliss.API.AppointmentManagement.Domain.Repositories;
+
+public interface IUserRepository : IBaseRepository<User>
+{
+    
+}
