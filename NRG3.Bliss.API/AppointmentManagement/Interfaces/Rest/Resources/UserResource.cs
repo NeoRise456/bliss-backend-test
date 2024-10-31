adeba3c7e02a125100ed4f5b8d52aebdde11381a
NeoRise456 <neorise456@gmail.com>
2024-10-31 13:30:50 -0500

    feat(resources): added user resource.

---
+++ NRG3.Bliss.API/AppointmentManagement/Interfaces/Rest/Resources/UserResource.cs
@@ -0,0 +1,14 @@
+﻿namespace NRG3.Bliss.API.AppointmentManagement.Interfaces.Rest.Resources;
+
+public record UserResource(
+    int Id,
+    string FirstName,
+    string LastName,
+    string Password,
+    string Email,
+    string Phone,
+    string Dni,
+    string Address,
+    string City,
+    DateTime BirthDate
+    );
