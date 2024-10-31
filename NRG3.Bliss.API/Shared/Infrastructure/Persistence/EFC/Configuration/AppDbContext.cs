6118012c688e1b7b9c5423a8d00cb893c476cbfb
NeoRise456 <neorise456@gmail.com>
2024-10-31 13:36:45 -0500

    feat(appdbcontext): added user aggregate to table relation.

--- NRG3.Bliss.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ NRG3.Bliss.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,6 +1,7 @@
 using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
 using Microsoft.EntityFrameworkCore;
 using NRG3.Bliss.API.AppointmentManagement.Domain.Model.Aggregates;
+using NRG3.Bliss.API.AppointmentManagement.Domain.Model.Entities;
 using NRG3.Bliss.API.ServiceManagement.Domain.Model.Aggregates;
 using NRG3.Bliss.API.ServiceManagement.Domain.Model.Entities;
 using NRG3.Bliss.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
@@ -46,6 +47,20 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
         builder.Entity<Appointment>().Property(a => a.ReservationDate).IsRequired();
         builder.Entity<Appointment>().Property(a => a.ReservationStartTime).IsRequired();
         builder.Entity<Appointment>().Property(a => a.Requirements).IsRequired();
+
+        builder.Entity<User>().HasKey(u => u.Id);
+        builder.Entity<User>().Property(u => u.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<User>().Property(u => u.FirstName).IsRequired().HasMaxLength(50);
+        builder.Entity<User>().Property(u => u.LastName).IsRequired().HasMaxLength(50);
+        builder.Entity<User>().Property(u => u.Password).IsRequired().HasMaxLength(50);
+        builder.Entity<User>().Property(u => u.Email).IsRequired();
+        builder.Entity<User>().Property(u => u.Phone).IsRequired().HasMaxLength(9);
+        builder.Entity<User>().Property(u => u.Dni).IsRequired().HasMaxLength(8);
+        builder.Entity<User>().Property(u => u.Address).IsRequired();
+        builder.Entity<User>().Property(u => u.City).IsRequired().HasMaxLength(100);
+        builder.Entity<User>().Property(u => u.BirthDate).IsRequired();
+        
+        
         
         builder.Entity<Category>()
             .HasMany(c => c.Services)
@@ -58,6 +73,24 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
             .WithOne(s => s.Company)
             .HasForeignKey(s => s.CompanyId)
             .HasPrincipalKey(c => c.Id);
+        
+        builder.Entity<Company>()
+            .HasMany(c => c.Appointments)
+            .WithOne(a => a.Company)
+            .HasForeignKey(a => a.CompanyId)
+            .HasPrincipalKey(c => c.Id);
+        
+        builder.Entity<Service>()
+            .HasMany(s => s.Appointments)
+            .WithOne(a => a.Service)
+            .HasForeignKey(a => a.ServiceId)
+            .HasPrincipalKey(s => s.Id);
+
+        builder.Entity<User>()
+            .HasMany(u => u.Appointments)
+            .WithOne(a => a.User)
+            .HasForeignKey(a => a.UserId)
+            .HasPrincipalKey(u => u.Id);
         
         builder.UseSnakeCaseNamingConvention();
     }
