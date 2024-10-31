29f612d8f0bc8157653d9e69d6766d603d7dd9ac
NeoRise456 <neorise456@gmail.com>
2024-10-31 13:34:49 -0500

    feat(repositories): implemented appointment repository.

---
+++ NRG3.Bliss.API/AppointmentManagement/Infrastructure/Persistence/EFC/Repositories/AppointmentRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NRG3.Bliss.API.AppointmentManagement.Domain.Model.Aggregates;
+using NRG3.Bliss.API.AppointmentManagement.Domain.Repositories;
+using NRG3.Bliss.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using NRG3.Bliss.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace NRG3.Bliss.API.AppointmentManagement.Infrastructure.Persistence.EFC.Repositories;
+
+public class AppointmentRepository(AppDbContext context): BaseRepository<Appointment>(context), IAppointmentRepository
+{
+    public async Task<IEnumerable<Appointment>> FindAppointmentsByUserIdAsync(int userId) => 
+        await Context.Set<Appointment>()
+            .Include(a => a.User)
+            .Include(a => a.Company)
+            .Include(a=>a.Service)
+            .Where(a => a.UserId == userId).ToListAsync();
+    
+    
+}
