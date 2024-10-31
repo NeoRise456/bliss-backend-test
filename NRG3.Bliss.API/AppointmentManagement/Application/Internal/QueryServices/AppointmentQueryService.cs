f1417b842fc283e0bf5078f79a8491f23340b4fa
cwassointt <dillpixel1@gmail.com>
2024-10-31 02:12:29 -0500

    feat: added new Appointment Query Service.

---
+++ NRG3.Bliss.API/AppointmentManagement/Application/Internal/QueryServices/AppointmentQueryService.cs
@@ -0,0 +1,19 @@
+﻿using NRG3.Bliss.API.AppointmentManagement.Domain.Model.Aggregates;
+using NRG3.Bliss.API.AppointmentManagement.Domain.Model.Queries;
+using NRG3.Bliss.API.AppointmentManagement.Domain.Repositories;
+using NRG3.Bliss.API.AppointmentManagement.Domain.Services;
+
+namespace NRG3.Bliss.API.AppointmentManagement.Application.Internal.QueryServices;
+
+public class AppointmentQueryService(IAppointmentRepository appointmentRepository) : IAppointmentQueryService
+{
+    public async Task<IEnumerable<Appointment>> Handle(GetAllAppointmentsByUserIdQuery query)
+    {
+        return await appointmentRepository.FindByUserIdAsync(query.UserId);
+    }
+
+    public async Task<Appointment?> Handle(GetAppointmentByIdQuery query)
+    {
+        return await appointmentRepository.FindByIdAsync(query.Id);
+    }
+}
