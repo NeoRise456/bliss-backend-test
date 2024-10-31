a47d7328f26cfd9caf81f7caac77108f9a5b7f84
NeoRise456 <neorise456@gmail.com>
2024-10-31 00:47:26 -0500

    feat(resources): add create appointment resource.

---
+++ NRG3.Bliss.API/AppointmentManagement/Interfaces/Rest/Resources/CreateAppointmentResource.cs
@@ -0,0 +1,11 @@
+﻿namespace NRG3.Bliss.API.AppointmentManagement.Interfaces.Rest.Resources;
+
+public record CreateAppointmentResource(
+    int userId,
+    int serviceId,
+    int companyId, 
+    DateTime reservationDate,
+    string status, 
+    DateTime date, 
+    string time
+    );
