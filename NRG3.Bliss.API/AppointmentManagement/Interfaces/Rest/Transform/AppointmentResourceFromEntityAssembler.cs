da69f1c1113d37ad610372a0ccb9be55c1e85032
NeoRise456 <neorise456@gmail.com>
2024-10-31 13:33:33 -0500

    feat(transform): added appointment resource assembler

---
+++ NRG3.Bliss.API/AppointmentManagement/Interfaces/Rest/Transform/AppointmentResourceFromEntityAssembler.cs
@@ -0,0 +1,23 @@
+﻿using NRG3.Bliss.API.AppointmentManagement.Domain.Model.Aggregates;
+using NRG3.Bliss.API.AppointmentManagement.Interfaces.Rest.Resources;
+using NRG3.Bliss.API.ServiceManagement.Interfaces.REST.Transform;
+
+namespace NRG3.Bliss.API.AppointmentManagement.Interfaces.Rest.Transform;
+
+public static class AppointmentResourceFromEntityAssembler
+{
+    public static AppointmentResource ToResourceFromEntity(Appointment entity)
+    {
+        return new AppointmentResource(
+            entity.Id,
+            UserResourceFromEntityAssembler.ToResourceFromEntity(entity.User),
+            ServiceResourceFromEntityAssembler.ToResourceFromEntity(entity.Service),
+            CompanyResourceFromEntityAssembler.ToResourceFromEntity(entity.Company),
+            entity.CreatedDate,
+            entity.Status,
+            entity.ReservationDate,
+            entity.ReservationStartTime,
+            entity.Requirements
+        );
+    }
+}
