d25844e69ef53cd7501853686156e9c2ba1ff811
NeoRise456 <neorise456@gmail.com>
2024-10-31 13:38:25 -0500

    feat(transform): added user resource from entity assembler.

---
+++ NRG3.Bliss.API/AppointmentManagement/Interfaces/Rest/Transform/UserResourceFromEntityAssembler.cs
@@ -0,0 +1,23 @@
+﻿using NRG3.Bliss.API.AppointmentManagement.Domain.Model.Entities;
+using NRG3.Bliss.API.AppointmentManagement.Interfaces.Rest.Resources;
+
+namespace NRG3.Bliss.API.AppointmentManagement.Interfaces.Rest.Transform;
+
+public static class UserResourceFromEntityAssembler
+{
+    public static UserResource ToResourceFromEntity(User entity)
+    {
+        return new UserResource(
+            entity.Id,
+            entity.FirstName,
+            entity.LastName,
+            entity.Password,
+            entity.Email,
+            entity.Phone,
+            entity.Dni,
+            entity.Address,
+            entity.City,
+            entity.BirthDate
+        );
+    }
+}
