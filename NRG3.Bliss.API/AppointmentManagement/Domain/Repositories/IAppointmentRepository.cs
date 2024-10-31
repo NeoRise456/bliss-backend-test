29df3846a11c4545873a643ee67850f7f3cf22f0
NeoRise456 <neorise456@gmail.com>
2024-10-31 02:04:00 -0500

    feat(repositories): added appointment repository interface.

---
+++ NRG3.Bliss.API/AppointmentManagement/Domain/Repositories/IAppointmentRepository.cs
@@ -0,0 +1,8 @@
+﻿using NRG3.Bliss.API.AppointmentManagement.Domain.Model.Aggregates;
+
+namespace NRG3.Bliss.API.AppointmentManagement.Domain.Repositories;
+
+public interface IAppointmentRepository
+{
+    Task<IEnumerable<Appointment>> FindAppointmentsByUserIdAsync(int userId);
+}
