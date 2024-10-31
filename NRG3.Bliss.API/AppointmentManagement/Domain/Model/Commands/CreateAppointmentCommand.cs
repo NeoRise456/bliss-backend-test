912c12fb2aa63335fa7029e4e03d265da66ade71
NeoRise456 <neorise456@gmail.com>
2024-10-31 01:01:35 -0500

    feat(commands): added create appointment command.

---
+++ NRG3.Bliss.API/AppointmentManagement/Domain/Model/Commands/CreateAppointmentCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace NRG3.Bliss.API.AppointmentManagement.Domain.Model.Commands;
+
+public record CreateAppointmentCommand( int UserId, int CompanyId, int ServiceId, string Status, DateTime ReservationDate, string ReservationStartTime, string Requirements);
