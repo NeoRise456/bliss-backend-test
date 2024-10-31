2df240515f20d819538e7885c6a14836026bc6e2
NeoRise456 <neorise456@gmail.com>
2024-10-31 01:42:00 -0500

    feat(aggregates): added Appointment Aggregate.

---
+++ NRG3.Bliss.API/AppointmentManagement/Domain/Model/Aggregates/Appointment.cs
@@ -0,0 +1,40 @@
+﻿using NRG3.Bliss.API.AppointmentManagement.Domain.Model.Commands;
+
+namespace NRG3.Bliss.API.AppointmentManagement.Domain.Model.Aggregates;
+
+public partial class Appointment
+{
+    public int Id { get; }
+    
+    public int UserId { get; private set; }
+    
+    public int CompanyId { get; private set; }
+    
+    public int ServiceId { get; private set; }
+    
+    public string Status { get; private set; }
+    
+    public DateTime ReservationDate { get; private set; }
+    
+    public string ReservationStartTime { get; private set; }
+    
+    public string Requirements { get; private set; }
+
+    public Appointment()
+    {
+        
+    }
+
+    public Appointment(CreateAppointmentCommand command)
+    {
+        UserId = command.UserId;
+        CompanyId = command.CompanyId;
+        ServiceId = command.ServiceId;
+        Status = command.Status;
+        ReservationDate = command.ReservationDate;
+        ReservationStartTime = command.ReservationStartTime;
+        Requirements = command.Requirements;
+    }
+    
+    
+}
---
+++ NRG3.Bliss.API/AppointmentManagement/Domain/Model/Aggregates/AppointmentAuditable.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using EntityFrameworkCore.CreatedUpdatedDate.Contracts;
+
+namespace NRG3.Bliss.API.AppointmentManagement.Domain.Model.Aggregates;
+
+public partial class Appointment : IEntityWithCreatedUpdatedDate
+{
+    [Column("CreatedAt")] public DateTimeOffset? CreatedDate { get; set; }
+    [Column("CreatedAt")] public DateTimeOffset? UpdatedDate { get; set; }
+}
