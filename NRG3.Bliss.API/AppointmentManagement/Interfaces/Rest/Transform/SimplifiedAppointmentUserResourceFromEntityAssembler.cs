cfbd29bf2d320f4f86657932c7dac4f97aecfd43
NeoRise456 <neorise456@gmail.com>
2024-11-01 03:04:07 -0500

    feat(transform): added simplified user resource from entity assembler .

---
+++ NRG3.Bliss.API/AppointmentManagement/Interfaces/Rest/Transform/SimplifiedAppointmentUserResourceFromEntityAssembler.cs
@@ -0,0 +1,12 @@
+﻿using NRG3.Bliss.API.AppointmentManagement.Domain.Model.Entities;
+using NRG3.Bliss.API.AppointmentManagement.Interfaces.Rest.Resources;
+
+namespace NRG3.Bliss.API.AppointmentManagement.Interfaces.Rest.Transform;
+
+public static class SimplifiedAppointmentUserResourceFromEntityAssembler
+{
+    public static SimplifiedUserResource ToResourceFromEntity(User entity)
+    {
+        return new SimplifiedUserResource(entity.Id, entity.FirstName, entity.LastName, entity.Dni);
+    }
+}
