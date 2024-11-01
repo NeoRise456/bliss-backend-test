f667f11fc547e557d662f18987f14d6feeb3c177
NeoRise456 <neorise456@gmail.com>
2024-11-01 03:06:35 -0500

    feat(resources): added simplified resources to appointment resource.

--- NRG3.Bliss.API/AppointmentManagement/Interfaces/Rest/Resources/AppointmentResource.cs
+++ NRG3.Bliss.API/AppointmentManagement/Interfaces/Rest/Resources/AppointmentResource.cs
@@ -1,13 +1,14 @@
 ﻿
 using NRG3.Bliss.API.ServiceManagement.Interfaces.REST.Resources;
+using NRG3.Bliss.API.Shared.Interfaces.REST.Resources;
 
 namespace NRG3.Bliss.API.AppointmentManagement.Interfaces.Rest.Resources;
 
 public record AppointmentResource(
     int id,
-    int userId,
-    int serviceId,
-    int companyId, 
+    SimplifiedUserResource user,
+    SimplifiedServiceResource service,
+    SimplifiedCompanyResource company, 
     DateTimeOffset? reservationDate,
     string status, 
     DateTime date, 
