ad40fb11e51a119b284f4aca0e02486e241f3aa2
ElviaRV <elviamarcela14@gmail.com>
2024-11-01 17:31:47 -0500

    feat(query-services): "Add IReviewQueryService interface and implement query handling for reviews"

---
+++ NRG3.Bliss.API/ReviewManagement/Domain/Services/IReviewQueryService.cs
@@ -0,0 +1,32 @@
+﻿using NRG3.Bliss.API.ReviewManagement.Domain.Model.Aggregates;
+using NRG3.Bliss.API.ReviewManagement.Domain.Model.Queries;
+
+namespace NRG3.Bliss.API.ReviewManagement.Domain.Services;
+
+/// <summary>
+/// Review query service interface
+/// </summary>
+public interface IReviewQueryService
+{
+    /// <summary>
+    /// Handle get all reviews by user id query
+    /// </summary>
+    /// <param name="query">
+    /// The <see cref="GetAllReviewsByUserIdQuery"/> query
+    /// </param>
+    /// <returns>
+    /// The <see cref="IEnumerable{Review}"/> object with the reviews
+    /// </returns>
+    Task<IEnumerable<Review>> Handle(GetAllReviewsByUserIdQuery query);
+
+    /// <summary>
+    /// Handle get all reviews by company id query
+    /// </summary>
+    /// <param name="query">
+    /// The <see cref="GetAllReviewsByCompanyId"/> query
+    /// </param>
+    /// <returns>
+    /// The <see cref="IEnumerable{Review}"/> object with the reviews
+    /// </returns>
+    Task<IEnumerable<Review>> Handle(GetAllReviewsByCompanyId query);
+}
