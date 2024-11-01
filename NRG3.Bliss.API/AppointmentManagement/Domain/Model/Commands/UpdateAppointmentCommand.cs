2b95f3c6ace3e919950c993d0f74be4dd5f93847
cwassointt <dillpixel1@gmail.com>
2024-11-01 13:19:32 -0500

    feat: added new record command to update appointments.

---
+++ NRG3.Bliss.API/AppointmentManagement/Domain/Model/Commands/UpdateAppointmentCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace NRG3.Bliss.API.AppointmentManagement.Domain.Model.Commands;
+
+public record UpdateAppointmentCommand(
+    int AppointmentId,
+    int UserId,
+    int CompanyId,
+    int ServiceId,
+    string Status,
+    DateTime ReservationDate,
+    string ReservationStartTime,
+    string Requirements
+);
