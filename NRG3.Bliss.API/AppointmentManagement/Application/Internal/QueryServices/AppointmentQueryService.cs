6589f77198925c5a69328d5df8e164c881b7f18f
NeoRise456 <neorise456@gmail.com>
2024-11-01 03:05:56 -0500

    feat(query-services): added call to find appointment by id of appointment repository.

--- NRG3.Bliss.API/AppointmentManagement/Application/Internal/QueryServices/AppointmentQueryService.cs
+++ NRG3.Bliss.API/AppointmentManagement/Application/Internal/QueryServices/AppointmentQueryService.cs
@@ -14,6 +14,6 @@ public class AppointmentQueryService(IAppointmentRepository appointmentRepositor
 
     public async Task<Appointment?> Handle(GetAppointmentByIdQuery query)
     {
-        return await appointmentRepository.FindByIdAsync(query.AppointmentId);
+        return await appointmentRepository.FindAppointmentByIdAsync(query.AppointmentId);
     }
 }
