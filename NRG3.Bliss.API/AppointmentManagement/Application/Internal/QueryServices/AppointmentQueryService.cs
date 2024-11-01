6fc3f3c7fe28af65c83cb55536a088d15c72bc91
NeoRise456 <neorise456@gmail.com>
2024-11-01 03:32:18 -0500

    feat(query-services): added documentation for appointment query services.

--- NRG3.Bliss.API/AppointmentManagement/Application/Internal/QueryServices/AppointmentQueryService.cs
+++ NRG3.Bliss.API/AppointmentManagement/Application/Internal/QueryServices/AppointmentQueryService.cs
@@ -5,13 +5,21 @@ using NRG3.Bliss.API.AppointmentManagement.Domain.Services;
 
 namespace NRG3.Bliss.API.AppointmentManagement.Application.Internal.QueryServices;
 
+/// <summary>
+/// Appointment query service
+/// </summary>
+/// <param name="appointmentRepository">
+/// Appointment repository
+/// </param>
 public class AppointmentQueryService(IAppointmentRepository appointmentRepository) : IAppointmentQueryService
 {
+    /// <inheritdoc />
     public async Task<IEnumerable<Appointment>> Handle(GetAllAppointmentsByUserIdQuery query)
     {
         return await appointmentRepository.FindAppointmentsByUserIdAsync(query.UserId);
     }
 
+    /// <inheritdoc />
     public async Task<Appointment?> Handle(GetAppointmentByIdQuery query)
     {
         return await appointmentRepository.FindAppointmentByIdAsync(query.AppointmentId);
