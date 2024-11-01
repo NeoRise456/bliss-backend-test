93e428d4e567fd8f9f7cdf38b7cca310c0f8cc4b
ElviaRV <elviamarcela14@gmail.com>
2024-10-31 19:19:12 -0500

    fix(commands): added reserved at parameter create appointemnt command.

--- NRG3.Bliss.API/AppointmentManagement/Domain/Model/Commands/CreateAppointmentCommand.cs
+++ NRG3.Bliss.API/AppointmentManagement/Domain/Model/Commands/CreateAppointmentCommand.cs
@@ -1,3 +1,3 @@
 ﻿namespace NRG3.Bliss.API.AppointmentManagement.Domain.Model.Commands;
 
-public record CreateAppointmentCommand( int UserId, int CompanyId, int ServiceId, string Status, DateTime ReservationDate, string ReservationStartTime, string Requirements);
+public record CreateAppointmentCommand( int UserId, int CompanyId, int ServiceId,DateTime ReservedAt, string Status, DateTime ReservationDate, string ReservationStartTime, string Requirements);
