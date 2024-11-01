217bf0926f283fa9e7d789d176726f24c91df42e
NeoRise456 <neorise456@gmail.com>
2024-11-01 01:30:12 -0500

    fix(services): changed datatype of handle for delete appointment command to Task.

--- NRG3.Bliss.API/AppointmentManagement/Domain/Services/IAppointmentCommandService.cs
+++ NRG3.Bliss.API/AppointmentManagement/Domain/Services/IAppointmentCommandService.cs
@@ -6,5 +6,5 @@ namespace NRG3.Bliss.API.AppointmentManagement.Domain.Services;
 public interface IAppointmentCommandService
 {
     Task<Appointment?> Handle(CreateAppointmentCommand command);
-    void Handle(DeleteAppointmentCommand command);
+    Task Handle(DeleteAppointmentCommand command);
 }
