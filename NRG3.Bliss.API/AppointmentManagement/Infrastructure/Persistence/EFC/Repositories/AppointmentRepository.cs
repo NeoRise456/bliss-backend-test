397798e243aff34d6b38c47e69ecfef67964db91
NeoRise456 <neorise456@gmail.com>
2024-11-01 03:27:55 -0500

    feat(repositories): added documentation for repositories classes.

--- NRG3.Bliss.API/AppointmentManagement/Infrastructure/Persistence/EFC/Repositories/AppointmentRepository.cs
+++ NRG3.Bliss.API/AppointmentManagement/Infrastructure/Persistence/EFC/Repositories/AppointmentRepository.cs
@@ -6,8 +6,15 @@ using NRG3.Bliss.API.Shared.Infrastructure.Persistence.EFC.Repositories;
 
 namespace NRG3.Bliss.API.AppointmentManagement.Infrastructure.Persistence.EFC.Repositories;
 
+/// <summary>
+/// Repository for the Appointment entity
+/// </summary>
+/// <param name="context">
+/// The database context
+/// </param>
 public class AppointmentRepository(AppDbContext context): BaseRepository<Appointment>(context), IAppointmentRepository
 {
+    /// <inheritdoc />
     public async Task<IEnumerable<Appointment>> FindAppointmentsByUserIdAsync(int userId) => 
         await Context.Set<Appointment>()
             .Include(a => a.User)
@@ -15,6 +22,7 @@ public class AppointmentRepository(AppDbContext context): BaseRepository<Appoint
             .Include(a=>a.Service)
             .Where(a => a.UserId == userId).ToListAsync();
 
+    /// <inheritdoc />
     public async Task<Appointment?> FindByServiceIdAndTimeAsync(int serviceId, DateTime reservationDate, string reservationStartTime)
         => await Context.Set<Appointment>()
             .Include(a => a.User)
@@ -22,6 +30,7 @@ public class AppointmentRepository(AppDbContext context): BaseRepository<Appoint
             .Include(a=>a.Service)
             .FirstOrDefaultAsync(a => a.ServiceId == serviceId && a.ReservationDate == reservationDate && a.ReservationStartTime == reservationStartTime);
 
+    /// <inheritdoc />
     public async Task<Appointment?> FindByUserIdAndTimeAsync(int userId, DateTime reservationDate, string reservationStartTime)
     => await Context.Set<Appointment>()
         .Include(a => a.User)
@@ -29,6 +38,7 @@ public class AppointmentRepository(AppDbContext context): BaseRepository<Appoint
         .Include(a=>a.Service)
         .FirstOrDefaultAsync(a => a.UserId == userId && a.ReservationDate == reservationDate && a.ReservationStartTime == reservationStartTime);
 
+    /// <inheritdoc />
     public async Task<Appointment?> FindAppointmentByIdAsync(int appointmentId)
     => await Context.Set<Appointment>()
         .Include(a => a.User)
