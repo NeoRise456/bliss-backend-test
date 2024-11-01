b58498299570110dae1e0353b80e634bb8095137
NeoRise456 <neorise456@gmail.com>
2024-11-01 01:55:25 -0500

    feat(repositories): implemented find by service id and time , and find by user d and time.

--- NRG3.Bliss.API/AppointmentManagement/Infrastructure/Persistence/EFC/Repositories/AppointmentRepository.cs
+++ NRG3.Bliss.API/AppointmentManagement/Infrastructure/Persistence/EFC/Repositories/AppointmentRepository.cs
@@ -14,6 +14,18 @@ public class AppointmentRepository(AppDbContext context): BaseRepository<Appoint
             .Include(a => a.Company)
             .Include(a=>a.Service)
             .Where(a => a.UserId == userId).ToListAsync();
-    
-    
+
+    public async Task<Appointment?> FindByServiceIdAndTimeAsync(int serviceId, DateTime reservationDate, string reservationStartTime)
+        => await Context.Set<Appointment>()
+            .Include(a => a.User)
+            .Include(a => a.Company)
+            .Include(a=>a.Service)
+            .FirstOrDefaultAsync(a => a.ServiceId == serviceId && a.ReservationDate == reservationDate && a.ReservationStartTime == reservationStartTime);
+
+    public async Task<Appointment?> FindByUserIdAndTimeAsync(int userId, DateTime reservationDate, string reservationStartTime)
+    => await Context.Set<Appointment>()
+        .Include(a => a.User)
+        .Include(a => a.Company)
+        .Include(a=>a.Service)
+        .FirstOrDefaultAsync(a => a.UserId == userId && a.ReservationDate == reservationDate && a.ReservationStartTime == reservationStartTime);
 }
