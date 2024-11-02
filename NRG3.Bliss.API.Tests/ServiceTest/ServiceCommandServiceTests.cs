34b250f1a7eae9a9b70f4b8b604f906a9e57deaf
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-11-02 01:47:12 -0500

    feat(service-command-service-test): add a test for service creation that validates that a service was created successfully.

--- NRG3.Bliss.API.Tests/ServiceTest/ServiceCommandServiceTests.cs
+++ NRG3.Bliss.API.Tests/ServiceTest/ServiceCommandServiceTests.cs
@@ -1,9 +1,9 @@
 
-
-
 using Moq;
 using NRG3.Bliss.API.ServiceManagement.Application.Internal.CommandServices;
+using NRG3.Bliss.API.ServiceManagement.Domain.Model.Aggregates;
 using NRG3.Bliss.API.ServiceManagement.Domain.Model.Commands;
+using NRG3.Bliss.API.ServiceManagement.Domain.Model.Entities;
 using NRG3.Bliss.API.ServiceManagement.Domain.Repositories;
 using NRG3.Bliss.API.Shared.Domain.Repositories;
 
@@ -49,6 +49,36 @@ namespace NRG3.Bliss.API.Tests.ServiceTest
             // [Then]: An InvalidOperationException is thrown with the message "The service name already exists for the company and category."
             Assert.Equal("The service name already exists for the company and category.", exception.Message);
         }
+        [Fact]
+        public async Task Handle_ShouldCreateService_WhenServiceNameIsUnique()
+        {
+            var command = new CreateServiceCommand(
+                1,
+                1,
+                "Unique Service",
+                "Description the service",
+                100,
+                30);
+            var category = new Category { Id = 1,Name = "Category1"};
+            var company = new Company { Id = 1,Name = "Company1"};
+            _serviceRepositoryMock.Setup(repo =>
+                repo.ServiceNameExistsForCompanyAndCategoryAsync(command.CompanyId, command.CategoryId,
+                    command.ServiceName)).ReturnsAsync(false);
+            _categoryRepositoryMock.Setup(repo => repo.FindByIdAsync(command.CategoryId)).ReturnsAsync(category);
+            _companyRepositoryMock.Setup(repo => repo.FindByIdAsync(command.CompanyId)).ReturnsAsync(company);
+            _serviceRepositoryMock.Setup(repo => repo.AddAsync(It.IsAny<Service>())).Returns(Task.CompletedTask);
+            _unitOfWorkMock.Setup(uow => uow.CompleteAsync()).Returns(Task.CompletedTask);
+
+            var result = await _serviceCommandService.Handle(command);
+            Assert.NotNull(result);
+            Assert.Equal(command.CompanyId,result.CompanyId);
+            Assert.Equal(command.CategoryId,result.CategoryId);
+            Assert.Equal(command.ServiceName,result.ServiceName);
+            Assert.Equal(command.Description,result.Description);
+            Assert.Equal(command.Price,result.Price);
+            Assert.Equal(command.Duration, result.Duration);
+        }
+
 
         
 
