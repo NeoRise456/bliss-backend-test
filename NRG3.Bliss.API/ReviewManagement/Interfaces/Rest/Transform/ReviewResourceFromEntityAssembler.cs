5e466fc6ac351ec4dc3902d819c1f7603c8ae76e
ElviaRV <elviamarcela14@gmail.com>
2024-11-01 23:34:00 -0500

    feat: Implement ReviewResourceFromEntityAssembler for transforming Review entities

---
+++ NRG3.Bliss.API/ReviewManagement/Interfaces/Rest/Transform/ReviewResourceFromEntityAssembler.cs
@@ -0,0 +1,36 @@
+﻿using NRG3.Bliss.API.ReviewManagement.Domain.Model.Aggregates;
+using NRG3.Bliss.API.ReviewManagement.Interfaces.Rest.Resources;
+using NRG3.Bliss.API.ReviewManagement.Interfaces.Rest.Transform;
+
+namespace NRG3.Bliss.API.ReviewManagement.Interfaces.Rest.Transform;
+
+/// <summary>
+/// Assembler to create a ReviewResource from a Review entity
+/// </summary>
+public static class ReviewResourceFromEntityAssembler
+{
+    /// <summary>
+    /// Assembles a ReviewResource from a Review entity
+    /// </summary>
+    /// <param name="review">
+    /// The <see cref="Review"/> entity to assemble the resource from
+    /// </param>
+    /// <returns>
+    /// The <see cref="ReviewResource"/> resource assembled from the entity
+    /// </returns>
+    public static ReviewResource ToResourceFromEntity(Review review)
+    {
+        var simplifiedAppointmentResource = SimplifiedReviewAppointmentFromEntityAssembler.ToResourceFromEntity(review.Appointment);
+
+        return new ReviewResource(
+            review.Id,
+            review.UserId,
+            simplifiedAppointmentResource,
+            review.ReviewDate,
+            review.ReviewUpdateDate,
+            review.Rating,
+            review.Comment,
+            review.ImageUrl
+        );
+    }
+}
