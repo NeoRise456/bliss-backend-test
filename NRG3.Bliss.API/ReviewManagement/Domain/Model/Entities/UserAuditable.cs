7d6713fbfb77240a11e1870e651fd89339aadb4b
ElviaRV <elviamarcela14@gmail.com>
2024-11-01 23:44:58 -0500

    fix(user): delete unnecessary entity

--- NRG3.Bliss.API/ReviewManagement/Domain/Model/Entities/UserAuditable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace NRG3.Bliss.API.ReviewManagement.Domain.Model.Entity;
-
-public class UserAuditable
-{
-    [Column("CreatedAt")] public DateTimeOffset? CreatedDate { get; set; }
-    [Column("CreatedAt")] public DateTimeOffset? UpdatedDate { get; set; }
-}
