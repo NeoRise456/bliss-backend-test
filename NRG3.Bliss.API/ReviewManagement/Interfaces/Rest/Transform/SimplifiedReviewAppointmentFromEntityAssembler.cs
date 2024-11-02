153239651abd3893ea64ead36707c57016cc9bca
ElviaRV <elviamarcela14@gmail.com>
2024-11-01 23:34:35 -0500

    feat: Implement SimplifiedReviewAppointmentFromEntityAssembler for transforming Appointment entities

---
+++ NRG3.Bliss.API/ReviewManagement/Interfaces/Rest/Transform/SimplifiedReviewAppointmentFromEntityAssembler.cs
@@ -0,0 +1,17 @@
+﻿using NRG3.Bliss.API.AppointmentManagement.Domain.Model.Aggregates;
+using NRG3.Bliss.API.ReviewManagement.Interfaces.Rest.Resources;
+
+namespace NRG3.Bliss.API.ReviewManagement.Interfaces.Rest.Transform;
+
+public static class SimplifiedReviewAppointmentFromEntityAssembler
+{
+    public static SimplifiedAppointmentResource ToResourceFromEntity(Appointment appointment)
+    {
+        return new SimplifiedAppointmentResource(
+            appointment.ServiceId,
+            appointment.CompanyId,
+            appointment.ReservationStartTime,
+            appointment.UserId
+        );
+    }
+}
