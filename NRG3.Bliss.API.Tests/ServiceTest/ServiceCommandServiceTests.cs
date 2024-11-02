d81ccd559ced9d94744bc03dba8b1c558d2edfda
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2024-11-02 01:43:33 -0500

    feat(service-command-service-test): add test for Service Creation validating that the condition of not adding a service that already exists in a company category works.

--- NRG3.Bliss.API.Tests/ServiceTest/ServiceCommandServiceTests.cs
+++ NRG3.Bliss.API.Tests/ServiceTest/ServiceCommandServiceTests.cs
@@ -3,6 +3,7 @@
 
 using Moq;
 using NRG3.Bliss.API.ServiceManagement.Application.Internal.CommandServices;
+using NRG3.Bliss.API.ServiceManagement.Domain.Model.Commands;
 using NRG3.Bliss.API.ServiceManagement.Domain.Repositories;
 using NRG3.Bliss.API.Shared.Domain.Repositories;
 
@@ -27,6 +28,26 @@ namespace NRG3.Bliss.API.Tests.ServiceTest
                 _categoryRepositoryMock.Object, 
                 _companyRepositoryMock.Object,
                 _unitOfWorkMock.Object);
+        }
+        // [Scenario]: Attempting to create a service with a duplicate name
+        [Fact]
+        public async Task Handle_WhenServiceNameIsNotUnique()
+        {
+            // [Given]: that a new service is to be created
+            var commad = new CreateServiceCommand(
+                1,
+                1,
+                "DuplicateServiceName",
+                "DescriptionService",
+                100,
+                30
+                );
+            // [When]: service data is passed with an existing name for the company and category.
+            _serviceRepositoryMock.Setup(repo =>
+                repo.ServiceNameExistsForCompanyAndCategoryAsync(commad.CompanyId, commad.CategoryId, commad.ServiceName)).ReturnsAsync(true);
+            var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => _serviceCommandService.Handle(commad));
+            // [Then]: An InvalidOperationException is thrown with the message "The service name already exists for the company and category."
+            Assert.Equal("The service name already exists for the company and category.", exception.Message);
         }
 
         
